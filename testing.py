4f86f463747328af9401065910dbad8a9a887a03
Abyaz <58655276+MuhammadAbyaz@users.noreply.github.com>
2023-11-04 15:27:20 +0500

    modified test cases

--- testing.py
+++ testing.py
@@ -5,3 +5,4 @@
 # Functional Test operate on units of functionality to make sure specific function of the software is working which may involve several units of software or whole systm working together
 
 # CI/CD continuous integration / continuous deployment which means that all the tests run automatically before merging the code in the master branch
+# usually test live in a tests file, or module not inline
